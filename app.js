fb89e7d37beaab6f982669953e049f72ec35af05
Estefan Hu <estefanhu074@gmail.com>
2019-12-17 20:40:24 -0800

    Added boilerplate

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
