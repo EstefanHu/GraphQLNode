75a8e767717fbdcc113ce8a26839bfe8f3846e36
Estefan Hu <estefanhu074@gmail.com>
2019-12-17 21:08:17 -0800

    installed base GraphQL

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
+var { graphql, buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!' };
+
+graphql(schema, '{ hello }', root).then((response) => {
+  console.log(response);
+});
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
